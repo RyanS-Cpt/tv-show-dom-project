38a7caeb17c6244174341a24ea5346a61fa5e451
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-04-03 19:44:49 +0200

    Edit search functionality

--- script.js
+++ script.js
@@ -13,22 +13,24 @@ function makePageForEpisodes(episodeList) {
   let search = document.getElementById("search");
   
   // only matching name or summary should be included - ?filter with includes method
-  // if (search.value.length > 0){
   // search should be case insensitive
-  function filter (array){
+  function filter (){
     let result = search.value.toLowerCase();
     console.log(result);
-    array.filter( el=> el.name.includes(result) || el.summary.includes(result));
+    let newList = episodeList.filter( el=> el.name.includes(result) || el.summary.includes(result));
+    console.log(newList);
+    return newList;
+  }
+  
+  for (let index of episodeList){
+        search.addEventListener("input", filter);
   }
   // search input should be live/immediate response after keystroke - add event-listener 'input' to respond to every change in the value 
   // display number of matching results next to search input
   // if search input is cleared all episodes should be displayed
-  // }
-  // else if (search.value.length === 0){
 
     for (let episode of episodeList){
-
-      search.addEventListener("keyup", filter(episodeList));
+          if (search.value.length === 0){
 
       let container = document.createElement("div");
       let pEl = document.createElement("p");
@@ -49,8 +51,29 @@ function makePageForEpisodes(episodeList) {
       container.appendChild(pEl);
       container.appendChild(imgEl);
       container.appendChild(summaryP);
+    } else if (search.value.length > 0){
+        
+       let container = document.createElement("div");
+      let pEl = document.createElement("p");
+      rootElem.appendChild(container);
+      if(episode.season <10 && episode.number < 10){
+        pEl.textContent = `${episode.name}, S0${episode.season}E0${episode.number}`;
+      }else if(episode.season < 100 && episode.number < 10){
+        pEl.textContent = `${episode.name}, S${episode.season}E0${episode.number}`;
+      }else if(episode.number < 100 && episode.season <10){
+          pEl.textContent = `${episode.name}, S0${episode.season}E${episode.number}`;
+      }
+      let imgEl = document.createElement("img");
+      imgEl.src = episode.image.medium;
+      rootElem.appendChild(imgEl);
+      let summaryP = document.createElement("p");
+      rootElem.appendChild(summaryP);
+      summaryP.innerHTML = episode.summary;
+      container.appendChild(pEl);
+      container.appendChild(imgEl);
+      container.appendChild(summaryP);
     }
-  // }
+  }
 }
 
 window.onload = setup;
