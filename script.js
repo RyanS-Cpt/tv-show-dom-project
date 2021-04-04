fa758ab8ad339cfa79b46c9ea41c4bfd05a6669d
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-04-04 20:23:39 +0200

    Added search result text. attempt search function

--- script.js
+++ script.js
@@ -1,59 +1,46 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
-
-function makePageForEpisodes(episodeList) {
+  let search = document.getElementById("search");
+  let searchResult = document.getElementById("searchResult");
   const rootElem = document.getElementById("root");
-    // console.log(episodeList[0]);
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const allEpisodes = getAllEpisodes();
+
 
   // add search input with type text and append to body - added in HTML
-  let search = document.getElementById("search");
-  
-  // only matching name or summary should be included - ?filter with includes method
+  // only matching name or summary should be included - ?filter with includes method - done in event listener
   // search should be case insensitive
-  function filter (){
-    let result = search.value.toLowerCase();
-    console.log(result);
-    let newList = episodeList.filter( el=> el.name.includes(result) || el.summary.includes(result));
-    console.log(newList);
-    return newList;
-  }
-  
-  for (let index of episodeList){
-        search.addEventListener("input", filter);
-  }
   // search input should be live/immediate response after keystroke - add event-listener 'input' to respond to every change in the value 
   // display number of matching results next to search input
   // if search input is cleared all episodes should be displayed
 
+function setup() {
+  // const allEpisodes = getAllEpisodes();
+console.log(allEpisodes);
+
+  makePageForEpisodes(allEpisodes);
+}
+
+let newList = [];
+
+search.addEventListener("input", ()=>{
+      let result = search.value.toLowerCase();
+      console.log(result);
+      newList = allEpisodes.filter( (el)=> {
+        return (
+              el.name.toLowerCase().includes(result) || 
+              el.summary.toLowerCase().includes(result)
+        );
+      });
+      console.log(newList);
+      makePageForEpisodes(newList);
+      searchResult.innerText = `Displaying ${newList.length}/${allEpisodes.length}`;
+    });
+
+function makePageForEpisodes(episodeList) {
+
     for (let episode of episodeList){
-          if (search.value.length === 0){
 
       let container = document.createElement("div");
-      let pEl = document.createElement("p");
-      rootElem.appendChild(container);
-      if(episode.season <10 && episode.number < 10){
-        pEl.textContent = `${episode.name}, S0${episode.season}E0${episode.number}`;
-      }else if(episode.season < 100 && episode.number < 10){
-        pEl.textContent = `${episode.name}, S${episode.season}E0${episode.number}`;
-      }else if(episode.number < 100 && episode.season <10){
-          pEl.textContent = `${episode.name}, S0${episode.season}E${episode.number}`;
-      }
-      let imgEl = document.createElement("img");
-      imgEl.src = episode.image.medium;
-      rootElem.appendChild(imgEl);
-      let summaryP = document.createElement("p");
-      rootElem.appendChild(summaryP);
-      summaryP.innerHTML = episode.summary;
-      container.appendChild(pEl);
-      container.appendChild(imgEl);
-      container.appendChild(summaryP);
-    } else if (search.value.length > 0){
-        
-       let container = document.createElement("div");
+      container.className = "container";
       let pEl = document.createElement("p");
       rootElem.appendChild(container);
       if(episode.season <10 && episode.number < 10){
@@ -74,7 +61,7 @@ function makePageForEpisodes(episodeList) {
       container.appendChild(summaryP);
     }
   }
-}
+
 
 window.onload = setup;
 
